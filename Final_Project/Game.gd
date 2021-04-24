6a7660e62cca877ebf835b03a5d75379f74b1306
wyemicha <77749681+wyemicha@users.noreply.github.com>
2021-04-23 23:09:59 -0400

    Added health bars to all chars

--- Final_Project/Game.gd
+++ Final_Project/Game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 var Enemy1 = load("res://Enemies/Enemy1.tscn")
-var Player = load("res://Player/Player2.tscn")
+var Player = load("res://Player/Player3.tscn")
 onready var player_camera = get_node("/root/Game/Camera2D")
 
 func _ready():
