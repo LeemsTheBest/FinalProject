605f25ea47bec895a10be8a12a389529615bda1f
puckalex <77993843+puckalex@users.noreply.github.com>
2021-04-24 13:56:13 -0400

    Enemies take and deal damage

--- Final_Project/Enemies/Enemy.gd
+++ Final_Project/Enemies/Enemy.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
-var speed = 1.5
+var hp = 150
+var speed = 100
 onready var Player_detect = $PlayerDetection
 var aggro = false
+var damage = 25
 
 var velocity = Vector2.ZERO
 
@@ -11,10 +13,20 @@ func _physics_process(delta):
 		look_at(Global.cur_player_instance.global_position)
 		velocity = Vector2(speed,0).rotated(rotation)
 		if position.distance_to(Global.cur_player_instance.global_position) > 5:
-			self.global_position += velocity
+			velocity = move_and_slide(velocity)
 
 func _on_PlayerDetection_body_entered(body):
 	aggro = true
 
 func _on_PlayerDetection_body_exited(body):
 	aggro = false
+
+func take_damage(damage):
+	hp -= damage
+	aggro = true
+	if (hp <= 0):
+		queue_free()
+
+func _on_AttackRadius_body_entered(body):
+	if body == Global.cur_player_instance:
+		body.take_damage(damage)
--- Final_Project/Game.gd
+++ Final_Project/Game.gd
@@ -2,12 +2,11 @@ extends Node2D
 
 
 var Enemy1 = load("res://Enemies/Enemy1.tscn")
-var Player = load("res://Player/Player3.tscn")
+var Player = load("res://Player/Player.tscn")
 onready var player_camera = get_node("/root/Game/Camera2D")
 
 func _ready():
 	var Player_inst = Player.instance()
-	
 	Player_inst.global_position = player_camera.global_position
 	Global.cur_player_instance = Player_inst
 	add_child(Player_inst)
