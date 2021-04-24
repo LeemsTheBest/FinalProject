d33c5499b85322bb149af8b6f1c470084e2e634a
wyemicha <77749681+wyemicha@users.noreply.github.com>
2021-04-24 14:39:48 -0400

    Added game_over and begin scene

--- Final_Project/Global.gd
+++ Final_Project/Global.gd
@@ -4,3 +4,19 @@ var player_health = 100
 var num_enemies = 0
 var max_enemies = 1
 var cur_player_instance = null
+var score = 0
+var timer = 0
+func update_score(s):
+	score += s
+	get_node("/root/Game/UI/Score").text = "Score: +str(score)"
+	
+
+	var t_m = floor(timer/60.0)
+	var t_s = timer % 60
+	var t = "Time: %02d" % t_m
+	t += "%02d" % t_s
+	get_node("root/Game/UI/Time").text = t
+
+func reset():
+	score = 0
+	update_score(0)
