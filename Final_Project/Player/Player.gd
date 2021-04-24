65166693bd5263f68637552d11dc8917338f5010
wyemicha <77749681+wyemicha@users.noreply.github.com>
2021-04-23 22:52:53 -0400

    Changed movement ability

--- Final_Project/Player/Player.gd
+++ Final_Project/Player/Player.gd
@@ -25,38 +25,8 @@ func _ready():
 	timer.connect("timeout",self,"on_timeout_complete")
 	add_child(timer)
 	
-func _physics_process(_delta):
+func _physics_process(delta):
 	
-	#Player movement
-	velocity += get_input()*speed
-	var current_speed = velocity.length()
-	position += velocity
-	velocity = velocity.normalized() * clamp(current_speed, -max_speed, max_speed)
-	velocity = move_and_slide(velocity, Vector2.UP, true)
-	#Player aim
-	look_at(get_global_mouse_position())
-	#Player shoot
-	if (Input.is_action_pressed("shoot") &&can_shoot):
-	#Could be in shoot function like other play scripts
-		can_shoot = false
-		var bullet1_inst = Bullet1.instance()
-		#add_child(bullet1_inst)
-	
-		var target = get_global_mouse_position()
-		bullet1_inst.global_position = end_of_gun.global_position
-		var direction_to_mouse = end_of_gun.global_position.direction_to(target).normalized()
-		bullet1_inst.set_direction(direction_to_mouse)
-		bullet1_inst.global_rotation = end_of_gun.global_rotation +PI/2
-		get_tree().get_root().call_deferred("add_child",bullet1_inst)
-		timer.start()
-		animation_player.play("MuzzleFlash")
-	
-	 
-func on_timeout_complete():
-	can_shoot = true	
-	
-	
-func get_input():
 	var input_vector = Vector2.ZERO
 	if Input.is_action_pressed("left"):
 		input_vector += Vector2(-1,0)
@@ -67,8 +37,38 @@ func get_input():
 	if Input.is_action_pressed("down"):
 		input_vector += Vector2(0,1)
 	
+	input_vector = input_vector.normalized()
+	move_and_collide(input_vector*max_speed, delta)
+	#Player aim
+	look_at(get_global_mouse_position())
+	#Player shoot
+	if (Input.is_action_pressed("shoot") &&can_shoot):
+	#Could be in shoot function like other play scripts
+		shoot()
+		timer.start()
+		
 	return input_vector
+	 
+func on_timeout_complete():
+	can_shoot = true	
+	
+	
+
+	
 func take_damage(damage):
 	Global.player_health -= damage
 	if Global.player_health <= 0:
 		queue_free()
+
+func shoot():
+	can_shoot = false
+	var bullet1_inst = Bullet1.instance()
+		#add_child(bullet1_inst)
+	
+	var target = get_global_mouse_position()
+	bullet1_inst.global_position = end_of_gun.global_position
+	var direction_to_mouse = end_of_gun.global_position.direction_to(target).normalized()
+	bullet1_inst.set_direction(direction_to_mouse)
+	bullet1_inst.global_rotation = end_of_gun.global_rotation +PI/2
+	get_tree().get_root().call_deferred("add_child",bullet1_inst)
+	animation_player.play("MuzzleFlash")
--- Final_Project/Player/Player2.gd
+++ Final_Project/Player/Player2.gd
@@ -6,7 +6,7 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var speed = 0.5
-var max_speed = 6
+var max_speed = 7
 var timer = null
 var bullet_delay = 0.05
 var can_shoot = true
@@ -25,43 +25,7 @@ func _ready():
 	timer.set_wait_time(bullet_delay)
 	timer.connect("timeout",self,"on_timeout_complete")
 	add_child(timer)
-func _physics_process(_delta):
-	#Player movement
-	velocity += get_input()*speed
-	var current_speed = velocity.length()
-	position += velocity
-	velocity = velocity.normalized() * clamp(current_speed, -max_speed, max_speed)
-	velocity = move_and_slide(velocity, Vector2.UP, true)
-	#Player aim
-	look_at(get_global_mouse_position())
-	#Player shoot. Added a timer for gun so bullets can't be spammed
-	#When input is pressed the can_shoot val turns false disallowing the gun to 
-	#be fired again until timer is up.
-	if (Input.is_action_pressed("shoot") && can_shoot):
-		
-		can_shoot = false
-	#Instancing bullet into scene
-		var bullet2_inst = Bullet2.instance()
-		
-	#All below are position and rotation lines to position the bullet at the end of 
-	#the characters gun and make sure it is rotated properly.
-	#The bullet is then added as a child in the scene
-		var target = get_global_mouse_position()
-		bullet2_inst.global_position = end_of_gun.global_position
-		var direction_to_mouse = end_of_gun.global_position.direction_to(target).normalized()
-		bullet2_inst.set_direction(direction_to_mouse)
-		bullet2_inst.global_rotation = end_of_gun.global_rotation +PI/2
-		
-		get_tree().get_root().call_deferred("add_child",bullet2_inst)
-		#Starts timer after bullet is fired to prevent shooting until timer is up
-		timer.start()
-		animation_player.play("Muzzle_Flash")
-
-	
-	
-	
-	
-func get_input():
+func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	if Input.is_action_pressed("left"):
 		input_vector += Vector2(-1,0)
@@ -72,8 +36,44 @@ func get_input():
 	if Input.is_action_pressed("down"):
 		input_vector += Vector2(0,1)
 	
+	
+	#Player movement
+	input_vector = input_vector.normalized()
+	move_and_collide(input_vector*max_speed, delta)
+	#Player aim
+	look_at(get_global_mouse_position())
+	#Player shoot. Added a timer for gun so bullets can't be spammed
+	#When input is pressed the can_shoot val turns false disallowing the gun to 
+	#be fired again until timer is up.
+	if (Input.is_action_pressed("shoot") && can_shoot):
+		shoot()
+		timer.start()
+		
 	return input_vector
 	
+	
+	
+	
+
+	
+	
+func shoot():
+	can_shoot = false
+	#Instancing bullet into scene
+	var bullet2_inst = Bullet2.instance()
+		
+	#All below are position and rotation lines to position the bullet at the end of 
+	#the characters gun and make sure it is rotated properly.
+	#The bullet is then added as a child in the scene
+	var target = get_global_mouse_position()
+	bullet2_inst.global_position = end_of_gun.global_position
+	var direction_to_mouse = end_of_gun.global_position.direction_to(target).normalized()
+	bullet2_inst.set_direction(direction_to_mouse)
+	bullet2_inst.global_rotation = end_of_gun.global_rotation +PI/2
+		
+	get_tree().get_root().call_deferred("add_child",bullet2_inst)
+	#Starts timer after bullet is fired to prevent shooting until timer is up
+	animation_player.play("Muzzle_Flash")
 func on_timeout_complete():
 	can_shoot = true
 func take_damage(damage):
